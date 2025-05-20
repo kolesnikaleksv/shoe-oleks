81f5107c9dfcc677d10e34a7816092e3999e3bdd
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-05-20 16:49:16 +0300

    Add kind auth and a lot of layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { CircleUser, MenuIcon } from 'lucide-react';
+import DashboardNavigation from '@/components/dashboard/DashboardNavigation';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user || user.email !== 'kolesnikaleksv@gmail.com') {
+    return redirect('/');
+  }
+  console.log(user, 'user');
+  return (
+    <div className="flex flex-col w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <header className="sticky top-0 flex items-center justify-between gap-4 h-16 border-b bg-white">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <DashboardNavigation /> hello
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              className="shrink-0 md:hidden"
+              variant="outline"
+              size="icon"
+            >
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side="left"
+            className="w-[400px] sm:w-[540px] p-5"
+            aria-describedby="mobile menu"
+          >
+            <SheetTitle className="mt-5"></SheetTitle>
+            <SheetDescription aria-describedby={undefined}></SheetDescription>
+            <nav className="grid gap-6 text-lg font-medium">
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <LogoutLink>Logout</LogoutLink>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+      <main className="my-5">{children}</main>
+    </div>
+  );
+}
