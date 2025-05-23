861673b336f06ad08f9b83d67eb7fd615bcaba55
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-05-23 10:47:54 +0300

    Add prisma and sync it with neon db and kindauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Hello from new project - shoe-oleks</div>;
+import { Button } from '@/components/ui/button';
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+
+export default async function Home() {
+  return (
+    <div>
+      Hello from new project - shoe-oleks
+      <div>
+        <Button asChild>
+          <LoginLink>Login</LoginLink>
+        </Button>
+        <Button>
+          <LogoutLink>logout</LogoutLink>
+        </Button>
+        <RegisterLink>Sign up</RegisterLink>
+      </div>
+    </div>
+  );
 }
