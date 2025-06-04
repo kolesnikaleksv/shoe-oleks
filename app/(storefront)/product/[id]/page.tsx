2e945480f8c43bb250a5008112a273a58fec2e7d
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-06-04 13:35:30 +0300

    Add product page and Featured component

---
+++ app/(storefront)/product/[id]/page.tsx
@@ -0,0 +1,60 @@
+import FeaturedProducts from '@/components/storefront/FeaturedProducts';
+import ImageSlider from '@/components/storefront/ImageSlider';
+import { Button } from '@/components/ui/button';
+import prisma from '@/lib/prisma';
+import { ShoppingBag, StarIcon } from 'lucide-react';
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+export async function getData(productId: string) {
+  const data = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      images: true,
+      description: true,
+    },
+  });
+  if (!data) {
+    return notFound();
+  }
+  return data;
+}
+
+export default async function Product({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const product = await getData(id);
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
+        <ImageSlider images={product.images} />
+        <div>
+          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
+            {product.name}
+          </h1>
+          <p className="text-3xl mt-2 text-gray-900">${product.price}</p>
+          <div className="mt-3 flex items-center gap-1">
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+          </div>
+          <p className="text-base text-gray-700 mt-6">{product.description}</p>
+          <Button size="lg" className="w-full mt-5">
+            <ShoppingBag className="mr-4 h-5 w-5" />
+            Add to Cart
+          </Button>
+        </div>
+      </div>
+      <div className="mt-16">
+        <FeaturedProducts />
+      </div>
+    </>
+  );
+}
---
+++ components/storefront/FeaturedProducts.tsx
@@ -0,0 +1,39 @@
+import prisma from '@/lib/prisma';
+import React from 'react';
+import ProductCard from './ProductCard';
+
+async function getData() {
+  const data = await prisma.product.findMany({
+    where: {
+      status: 'published',
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      price: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return data;
+}
+
+export default async function FeaturedProducts() {
+  const data = await getData();
+
+  return (
+    <>
+      <h1 className="text-2xl font-extrabold tracking-tight">
+        Featured Products
+      </h1>
+      <div className="mt-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data.map((item) => (
+          <ProductCard key={item.id} item={item} />
+        ))}
+      </div>
+    </>
+  );
+}
